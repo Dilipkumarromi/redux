fb74dbdded61d6dd12641440e73cc98bcc9f46d5
romi.indan@gmail.com <romi.indan@gmail.com>
2024-07-03 00:19:52 +0530

    store upade

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
 
+import DisplayCounter from './components/DisplayCounter';
+import Controller from './components/Controller';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <><DisplayCounter /><div className='raw'>
+      <Controller/>
+    </div></>
   );
 }
 
